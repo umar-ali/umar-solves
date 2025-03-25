fe7edfd858deb37f5bb7e76717a4deb107b980d6
umar <aligani619@gmail.com>
2025-03-25 23:39:30 +0530

    set up remove dups from sorted arr

---
+++ leetcode/LT-EASY/inplace-remove-dups-sorted-arr.py
@@ -0,0 +1,8 @@
+
+#https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+def remove_dups(nums:list):
+    ...
+
+if __name__ =="__main__":
+    nums = [0,0,1,1,1,2,2,2, 3,3, 3,4]
+    print(remove_dups(nums))
