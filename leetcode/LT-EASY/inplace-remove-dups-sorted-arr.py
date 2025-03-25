a1076387738c752e15e773e607cc7510c0fe0620
umar <aligani619@gmail.com>
2025-03-26 00:33:06 +0530

    Optimize remove dups

--- leetcode/LT-EASY/inplace-remove-dups-sorted-arr.py
+++ leetcode/LT-EASY/inplace-remove-dups-sorted-arr.py
@@ -1,26 +1,22 @@
 
 #https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 def remove_dups(nums:list):
-    i = 1
-    dups = 0
     l = len(nums)
-
     if l <=1:
         return l
-        
-    while True:
-        if i + dups == l:
-            break
-        if nums[i-1] == nums[i]:
-            dup= nums.pop(i)
-            nums.append(dup)
-            dups+=1
-            continue
-        i +=1
-    return l - dups
+    i = 0
+    j = 1
+    while j < l:
+        if nums[i] == nums[j]:
+            j+=1
+        else:
+           nums[i+1] = nums[j]
+           i+=1
+           j+=1
+    return i+1
 
 
 if __name__ =="__main__":
     nums = [0,0,1,1,1,2,2,2, 3,3, 3,4]
     zeros = [0] * 10
-    print(remove_dups(zeros))
+    print(remove_dups(nums))
