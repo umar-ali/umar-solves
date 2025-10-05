4e06862c168bdf352e4464d0a867e6b9fc738cff
umar ali <aligani619@gmail.com>
2025-10-05 18:55:10 +0530

    chore: Add accepted soln to lt#10

--- leetcode/LT-HARD/10-regex-matching.py
+++ leetcode/LT-HARD/10-regex-matching.py
@@ -2,19 +2,25 @@
 
 def isMatch(s:str, p:str) -> bool:
 
-    # if p is empty, then result is True if s i empty, otherwise False
-    if not p:
-        return not s
+    memo = {} #Memoize the intermediate results
 
-    #Checking a char match in pattern
-    #f_match is true when s is non-empty and p[0] is either s[0] or '.' 
-    f_match = bool(s) and p[0] in {s[0], '.'}
-
-    if len(p) >= 2 and p[1] == '*':
-        return (isMatch(s, p[2:]) # matching pattern succeeding *
-                or f_match and isMatch(s[1:], p)) #for matching <c>*, f_match matched one and let's match s[1..sn]
-    else:
-        return f_match and isMatch(s[1:], p[1:]) #Handling all other cases.. i.e., we know f_match(0,0) what about [1..sn], [1..pn]
+    def dp(i:int, j:int) -> bool:
+        #check at end of pattern
+        if (i,j) not in memo:
+            #if j is at end, matched if  i should be at it end otherwise not matched
+            if j == len(p):  
+                ans = i == len(s)
+            else:
+                #Checking char at i against char at pat j
+                f_match = i < len(s) and p[j] in {s[i], '.'}
+                if j + 1 < len(p) and p[j+1] == '*':
+                    ans = (dp(i, j + 2) # matching pattern succeeding *
+                            or f_match and dp(i+1, j)) #for matching <c>*, f_match matched one and let's match s[1..sn]
+                else:
+                    ans = f_match and dp(i+1, j+1) #Handling all other cases.. i.e., we know f_match(0,0) what about [1..sn], [1..pn]
+            memo[i,j] = ans
+        return memo[i,j]
+    return dp(0,0)
 
 if __name__ == "__main__":
     print(isMatch("aa", "a"))   #False
