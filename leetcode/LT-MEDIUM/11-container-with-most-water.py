438668fa7854afb6c0b81dc8fad30fe07c91ff6d
umar ali <aligani619@gmail.com>
2025-10-06 08:28:28 +0530

    chore: little Optimised soln for lt#11 still `TLE` for sorted array input

--- leetcode/LT-MEDIUM/11-container-with-most-water.py
+++ leetcode/LT-MEDIUM/11-container-with-most-water.py
@@ -2,10 +2,12 @@
 
 def maxArea(height:list[int]) -> int:
     m = 0
+    last = -1
     for i in range(len(height)):
-        for j in range(i+1, len(height)):
-            m = max(m, min(height[i], height[j]) * (j - i))
-
+        if height[i] > last:
+            for j in range(i+1, len(height)):
+                m = max(m, min(height[i], height[j]) * (j - i))
+            last = height[i]
     return m
 
 
@@ -13,4 +15,7 @@ def maxArea(height:list[int]) -> int:
 
 if __name__ == "__main__":
     print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    a = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+    print(maxArea(a))
+    print(maxArea(a[::-1]))
     print(maxArea([1, 1]))
