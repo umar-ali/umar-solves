dfcf43b52ef48fbf1afc71d9bbeb0e8b67e74b81
umar ali <aligani619@gmail.com>
2025-10-08 08:06:37 +0530

    chore: Add Accepted soln to lt#17

--- leetcode/LT-MEDIUM/17-phone-number-combination.py
+++ leetcode/LT-MEDIUM/17-phone-number-combination.py
@@ -4,11 +4,38 @@
 # Return All possible letter combination from telephone buttons
 # "23" -> ["ad", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 
-def letterCombination(digits:str) -> list[str]:
-    pass
 
+def letterCombination(digits:str) -> list[str]:
+
+    digits_map = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+    }
+    
+    def permute(d:str, combinations:list[str] = []):
+        if len(combinations) == 0:
+            return [l for l in digits_map[d]] 
+        res = []
+        for l in digits_map[d]:
+            res += [l + c for c in combinations]
+        return res
+    
+    if len(digits) == 0:
+        return []
+
+    if len(digits) == 1:
+        return permute(digits)
+    return permute(digits[0], letterCombination(digits[1:]))
 
 if __name__ == "__main__":
-    pass
+    print(letterCombination(''))
+    print(letterCombination('2'))
+    print(letterCombination('23'))
 
 
