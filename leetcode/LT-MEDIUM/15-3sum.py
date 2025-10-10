1942c59bd4e53ea7ec8120a5b04a600d32625c52
umar ali <aligani619@gmail.com>
2025-10-10 06:38:40 +0530

    chore: Add Some Optimisation for lt#15  still tle

--- leetcode/LT-MEDIUM/15-3sum.py
+++ leetcode/LT-MEDIUM/15-3sum.py
@@ -22,5 +22,29 @@
 #                  solution.append(sublist)
 #         return solution
 
+from collections import defaultdict
+def threeSum(nums:list[int]) -> list[list[int]]:
+    n = len(nums)
+    res = []
+    sum2 = defaultdict(list)
+    for i in range(n):
+        for j in range(i+1, n):
+            sum2[nums[i]+nums[j]].append((i,j))
+    print(sum2)
+    for i in range(n):
+        op = nums[i] * -1
+        if op in sum2:
+            pairs = sum2[op]
+            for j in range(len(pairs)):
+                if i not in pairs[j]:
+                    r = sorted([nums[i], nums[pairs[j][0]], nums[pairs[j][1]]])
+                    if r not in res:
+                        res.append(r)
+    return res
+
+
 if __name__ == "__main__":
     # print(Solution().threeSum([-1, 0, 1, 2, -1, -4])) #[[-1, 0, 1], [-1, -1, 2]]
+    # print(threeSum([-1, 0, 1, 2, -1, -4])) #[[-1, 0, 1], [-1, -1, 2]]
+    print(threeSum([2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10]))
+    # print(threeSum([0,0,0])) #[[-1, 0, 1], [-1, -1, 2]]
