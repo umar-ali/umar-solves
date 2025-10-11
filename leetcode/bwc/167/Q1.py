e26b9fa68312f8de76f20d846ac2dec33e2b056f
umar ali <aligani619@gmail.com>
2025-10-11 21:32:53 +0530

    chore: Add accepted soln to lt bwc#167 Q1

---
+++ leetcode/bwc/167/Q1.py
@@ -0,0 +1,47 @@
+#Equal Score Substrings
+
+
+def scoreBalance(s:str) -> bool:
+    ln = len(s)
+    m = ln // 2
+
+    alpha = "*abcdefghijklmnopqrstuvwxyz"
+    alphaScore = {alpha[i]:i for i in range(len(alpha)) if alpha[i] != "*" }
+
+    scores = [alphaScore[c] for c in s]
+    
+
+    l = scores[:m]
+    r = scores[m:]
+
+    lsm = sum(l)
+    rsm = sum(r)
+    # print(lsm, rsm)
+    if lsm == rsm:
+        return True
+
+    i = 1
+    while i <= m:
+        lx = sum(scores[m:m+i])
+        rx = sum(scores[m-i:m])
+        # print(i, m, lsm, rsm, lx, rx)
+        if lsm + lx == rsm - lx:
+            return True
+        if lsm - rx == rsm + rx:
+            return True
+        i+=1
+
+    return False
+
+
+if __name__ == "__main__":
+    print(scoreBalance("adbc")) #True
+    print(scoreBalance("bace")) #False
+    print(scoreBalance("aab")) #True
+    print(scoreBalance("aaa")) #False
+    print(scoreBalance("abcdefghijk")) #False
+    print(scoreBalance("abccabcba")) #True
+    print(scoreBalance("abdcd")) #True
+    print(scoreBalance("ecbdaa")) #True
+    print(scoreBalance("acabcea")) #False
+
