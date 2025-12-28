874f5594377223c93fc4614cf22a3549ca4233c7
umar <aligani619@gmail.com>
2025-12-28 08:56:23 +0530

    Add accepted soln lt-wc#482 Q1

--- leetcode/wc/482/Q1.py
+++ leetcode/wc/482/Q1.py
@@ -2,16 +2,23 @@
 
 def maximumScore(nums:list[int]) -> int:
     ln = len(nums)
-    mx = None
+    pref_sum_table = [None for i in range(ln)]
+    suff_min_table = [None for i in range(ln)]
     for i in range(ln):
-        prefx = nums[:i+1]
-        suffx = nums[i+1:]
-        if not prefx or not suffx:
-            continue
-        prefix_sum = sum(prefx)
-        suffix_min = min(suffx) if suffx else 0
-        score = prefix_sum - suffix_min
-        print(i, prefix_sum, suffix_min, score)
+        if i == 0:
+            pref_sum_table[i] = nums[i]
+        else:
+            pref_sum_table[i] = pref_sum_table[i-1] + nums[i]
+        
+    for i in range(ln-1, -1, -1):
+        if i == ln-1:
+            suff_min_table[i] = nums[i]
+        else:
+            suff_min_table[i] = min(suff_min_table[i+1], nums[i])
+    
+    mx = None
+    for i in range(ln-1):
+        score = pref_sum_table[i] - suff_min_table[i+1]
         if mx is None or score > mx:
             mx = score
     return mx
@@ -22,6 +29,7 @@ if __name__ == "__main__":
     print(maximumScore( [10,-1,3,-4,-5]))
     print(maximumScore([-7, -5, 3]))
     print(maximumScore([1,1]))
+    print(maximumScore([1,1,-2]))
 
 
 
