3efbde7db9a1a0f16f620ad773cd6b9fcf6c2d1a
umar ali <aligani619@gmail.com>
2026-01-14 10:03:08 +0530

    Add failed tle soln to lt#160

---
+++ leetcode/LT-EASY/160-intersection-2-ll.py
@@ -0,0 +1,121 @@
+class ListNode:
+    def __init__(self, val=0, next = None):
+        self.val = val
+        self.next = next
+
+
+#-------helper funcs---------
+def build_linked_list_with_intersection(intersectVal, listA, listB):
+    headA = None
+    prevA = None
+    intersectNode = None
+    for i, e in enumerate(listA):
+        n = ListNode(e)
+        if headA is None:
+            headA = n
+            prevA = n
+        else:
+            prevA.next = n
+            prevA = n
+        if e == intersectVal:
+            intersectNode  = n
+
+    headB = None
+    prevB = None
+
+    for i, e in enumerate(listB):
+
+        if e == intersectVal:
+            prevB.next = intersectNode
+            break
+
+        n = ListNode(e)
+        if headB is None:
+            headB = n
+            prevB = n
+        else:
+            prevB.next = n
+            prevB = n
+
+    return headA, headB
+
+
+
+def print_ll(h:ListNode | None):
+    while h is not None:
+        print(h.val, end="")
+        if h.next is not None:
+            print(", ", end="")
+        else:
+            print()
+        h = h.next
+#-------------------------------
+
+
+def reverse(head: ListNode | None) -> ListNode | None:
+    curr = head
+    prev = None
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next 
+    return prev
+
+
+
+def getIntersectionNode(headA: ListNode | None, headB: ListNode | None) -> ListNode | None :
+    if headA is None or headB is None:
+        return None
+
+    revB = reverse(headB)
+
+    t = headA
+    while t.next:
+        t = t.next
+    t.next = revB #modification!!!
+
+    
+
+    fast = headA
+    slow = headA
+    
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            slow = headA
+
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next
+            return slow
+    return None
+
+def get_intersection_no_mod(headA: ListNode | None, headB: ListNode | None) -> ListNode | None :
+    if headA is None or  headB is None:
+        return None
+    
+    ptrA = headA
+
+    while ptrA:
+        ptrB = headB
+        while ptrB:
+            if ptrA == ptrB:
+                return ptrA
+            ptrB = ptrB.next
+        ptrA = ptrA.next
+    return None
+
+
+if __name__ == "__main__":
+    la = [1, 2, 3, 4]
+    lb = [5,3]
+    ival = 3
+    lla, llb = build_linked_list_with_intersection(ival, la, lb)
+
+    inter = get_intersection_no_mod(lla, llb)
+    if inter:
+        print(inter.val)
+    print(inter)
