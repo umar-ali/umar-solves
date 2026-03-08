d5f1b93579e004fc8fbf7072f9df7beb3947be1c
Umar <aligani619@gmail.com>
2026-03-08 16:47:10 +0530

    Add setup for $lc#57

---
+++ leetcode/LT-MEDIUM/57-insert-intervals.py
@@ -0,0 +1,7 @@
+
+
+def insert(intervals:list[list[int]], newIntervals:list[int]) -> list[list[int]]:
+    ...
+
+if __name__ == "__main__":
+    print(insert([[1,3],[6,9]], [2,5])) #  [[1,5],[6,9]]
