cbe9e1ef0eed0dc6174a7276677febd1beb8c796
Umar <aligani619@gmail.com>
2026-03-10 23:37:15 +0530

    Add Accopted soln lc#48

--- leetcode/LT-MEDIUM/48-rotate-image.py
+++ leetcode/LT-MEDIUM/48-rotate-image.py
@@ -1,9 +1,23 @@
 
 
 def rotate(matrix: list[list[int]]) -> None:
-    ...
+    #intuition
+    #swap matrix[i][j] with matrix[j][i] and vice versa
+    #reverse all the row
+
+    r = len(matrix)
+    c = len(matrix[0])
+
+    for i in range(r):
+        for j in range(i+1, c):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(r):
+        matrix[i].reverse()
 
 if __name__ == "__main__":
+    import pprint
     matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    pprint.pp(matrix)
     rotate(matrix)
     print(matrix)
