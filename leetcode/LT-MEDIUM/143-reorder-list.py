2913901a334c296aef3da9d52ab17cc9cf5888b1
Umar <aligani619@gmail.com>
2026-03-12 00:19:40 +0530

    Add accepted soln lc#143

--- leetcode/LT-MEDIUM/143-reorder-list.py
+++ leetcode/LT-MEDIUM/143-reorder-list.py
@@ -31,11 +31,48 @@ def print_ll(h:ListNode | None):
 
 
 def reorderList(head:Optional[ListNode]) -> None:
-    ...
+    # find the middle
+    slow = head 
+    fast = head 
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    h2 = slow.next
+    slow.next = None # made the the end end
+
+    # reverse from middle
+    t1 = None
+    t2 = None
+
+    while h2:
+        t2 = h2.next
+        h2.next = t1
+        t1 = h2
+        h2= t2
+
+    # merge them
+    ptr = head
+    tmp = t1
+    while tmp:
+        #store next
+        nxt_ptr = ptr.next
+        nxt_h2 = tmp.next
+
+
+        #attach
+        ptr.next = tmp
+        tmp.next = nxt_ptr
+
+        #set for next iteration
+        ptr = nxt_ptr
+        tmp = nxt_h2
 
 
 if __name__ == "__main__":
     ll = build_linked_list(*[1,2,3,4,5])
+    print_ll(ll)
     reorderList(ll)
     print_ll(ll)
     
