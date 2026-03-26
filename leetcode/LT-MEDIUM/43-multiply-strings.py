0c10e4c02275c2e4a63beea70eed59aa27823d2e
Umar <aligani619@gmail.com>
2026-03-26 11:13:12 +0530

    Add accepted and valid soln lc#43

--- leetcode/LT-MEDIUM/43-multiply-strings.py
+++ leetcode/LT-MEDIUM/43-multiply-strings.py
@@ -3,7 +3,21 @@
 
 
 def multiply(num1:str, num2:str) -> str:
-    return str(int(num1) * int(num2)) #direct conversion not allowed
+    n, m = len(num1), len(num2)
+
+    result = [0] * (n + m)
+
+    for i in range(n -1, -1 , -1):
+        for j in range(m -1, -1 , -1):
+            mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+            sum_ = mul + result[i + j + 1]
+            result[i + j + 1] = sum_ % 10 
+            result[i + j] += sum_ // 10
+
+    prod = ''.join(map(str, result)).lstrip('0')
+    return prod if prod else "0"
+
+
 
 
 if __name__ == "__main__":
